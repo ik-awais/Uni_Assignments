d8749a5f12d8566d2fed78a5cfaee6b06241b03b
ik-awais <mawaisqq@gmail.com>
2026-04-18 11:04:10 +0500

    Working on my OOP Assignment 3

--- Theory/25P0011_MuhammadAwais_BAI2A_Assignment3.cpp
+++ Theory/25P0011_MuhammadAwais_BAI2A_Assignment3.cpp
@@ -4,8 +4,50 @@ class Vector{
 protected:
     int* array;
     int size;
-    int space;
+    int check;
 public:
+    Vector();
+    ~Vector();
     void resize();
-    
+    void push_back(int);
+    void getData();
+    void get(int);
+    void find_len();
 };
+Vector::Vector(){
+    size = 2;
+    check = 0;
+    array = new int[size];
+}
+void Vector::push_back(int num){
+    if(check>=size)
+        resize();
+    array[check] = num;
+    check++;
+}
+void Vector::resize(){
+    size*=2;
+    int* temp = array;
+    array = new int[size];
+    for(int i = 0; i < check; i++)
+        array[i] = temp[i];
+    delete[] temp;
+}
+void Vector::getData(){
+    cout << "Data of Vector: ";
+    for(int i = 0; i < check; i++)
+        cout << array[i] << " ";
+    cout << "\n";
+}
+void Vector::get(int index){
+    index<check ? 
+    cout << array[index] << "is at index[" << index << "]\n" :
+    cout << "Invalid index!\n";
+}
+void Vector::find_len(){
+    cout << "Length of Vector is: " << check << endl;
+}
+Vector::~Vector(){
+    delete[] array;
+    array = nullptr;
+}
