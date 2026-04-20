51dfdd207fc91a6befc1eb2e08e5ef380b2f9b4a
ik-awais <mawaisqq@gmail.com>
2026-04-20 22:19:15 +0500

    Updating Assignment 3 code

--- Theory/25P0011_MuhammadAwais_BAI2A_Assignment3.cpp
+++ Theory/25P0011_MuhammadAwais_BAI2A_Assignment3.cpp
@@ -6,16 +6,21 @@ protected:
     int size;
     int check;
 public:
-    Vector();
+    Vector(int);
     ~Vector();
-    void resize();
     void push_back(int);
-    void getData();
+    void resize();
     void get(int);
     void find_len();
+    void getData();
 };
-Vector::Vector(){
-    size = 2;
+Vector::Vector(int size){
+    if(size <= 0)
+    {
+        cout << "Invalid size!\n";
+        return;
+    }
+    this->size = size;
     check = 0;
     array = new int[size];
 }
@@ -33,12 +38,6 @@ void Vector::resize(){
         array[i] = temp[i];
     delete[] temp;
 }
-void Vector::getData(){
-    cout << "Data of Vector: ";
-    for(int i = 0; i < check; i++)
-        cout << array[i] << " ";
-    cout << "\n";
-}
 void Vector::get(int index){
     index<check ? 
     cout << array[index] << "is at index[" << index << "]\n" :
@@ -47,7 +46,44 @@ void Vector::get(int index){
 void Vector::find_len(){
     cout << "Length of Vector is: " << check << endl;
 }
+void Vector::getData(){
+    cout << "Data of Vector: ";
+    for(int i = 0; i < check; i++)
+        cout << array[i] << " ";
+    cout << "\n";
+}
 Vector::~Vector(){
     delete[] array;
     array = nullptr;
+}
+class Unique_Vector : public Vector{
+public:
+    Unique_Vector(int);
+    void push_back(int);
+};
+Unique_Vector::Unique_Vector(int size){
+    this->size = size;
+}
+class FrequencyVector : public Vector{
+    int* freqArray;
+    int freqsize;
+public:
+    FrequencyVector(int);
+    ~FrequencyVector();
+    void push_back(int);
+};
+FrequencyVector::FrequencyVector(int size){
+    this->size = size;
+}
+int main(){
+    int size = 0;
+    cin >> size;
+    int array[size];
+    for(int i = 0; i < size; i++)
+        cin >> array[i];
+    Vector v1(size);
+    cin >> size;
+    Unique_Vector v2(size);
+    cin >> size;
+    FrequencyVector v3(size);
 }
