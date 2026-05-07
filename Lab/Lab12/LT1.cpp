a04e45060ab6dd6ad92eeabba90a34df06fa4d80
ik-awais <mawaisqq@gmail.com>
2026-05-07 14:52:07 +0500

    Modified the Lab Task

--- Lab/Lab12/LT1.cpp
+++ Lab/Lab12/LT1.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 class PaymentSystem{
 protected:
-    double payment;
+    double payment, service_fee, refund_fee;
 public:
     virtual void pay() = 0;
     virtual void refund() = 0;
-};  
+};
 class EasyPaisa: public PaymentSystem{
 public:
     void pay()override;
@@ -15,11 +15,13 @@ void EasyPaisa::pay(){
     std::cout << "===EasyPaisa at your Service===\nEnter the amount to pay: ";
     std::cin >> payment;
     std::cout << "Service fee of 2% will be deducted!\n";
-    std::cout << "===Rs." << payment - (payment*0.02) << "transferred with Rs." << payment*0.02 << " as service fee===\nThankyou for using EasyPaisa!\n"; 
+    service_fee = payment*0.02;
+    std::cout << "===Rs." << payment - service_fee << " transferred with Rs." << service_fee << " as service fee===\nThankyou for using EasyPaisa!\n"; 
 }
 void EasyPaisa::refund(){
-    std::cout << "Amount is successfully refunded!\nAmount of " << payment - (payment*0.02)
-    << " is returned into your account.\n2% Service fee of Rs." << payment*0.02 << 
+    refund_fee = payment - service_fee;
+    std::cout << "Amount is successfully refunded!\nAmount of " << refund_fee
+    << " is returned into your account.\n2% Service fee of Rs." << service_fee << 
     " is deducted.\n===Keep using EasyPaisa and enjoy our services===\n";
 }
 class JazzCash:public PaymentSystem{
@@ -31,12 +33,14 @@ void JazzCash::pay(){
     std::cout << "===JazzCash at your Service===\nEnter the amount to pay: ";
     std::cin >> payment;
     std::cout << "Service fee of 5% will be deducted!\n";
-    std::cout << "===Rs." << payment - (payment*0.05) << " transferred with"
-    << "Rs." << payment*0.05 << "as service fee===Thankyou for using JazzCash\n"; 
+    service_fee = payment*0.05;
+    std::cout << "===Rs." << payment - service_fee << " transferred with"
+    << "Rs." << service_fee << " as service fee===\nThankyou for using JazzCash\n"; 
 }
 void JazzCash::refund(){
-    std::cout << "Amount is successfully refunded!\nAmount of " << payment - (payment*0.05)
-    << "is returned into your account.\n5% Service fee of " << payment*0.05 << 
+    refund_fee = payment - service_fee;
+    std::cout << "Amount is successfully refunded!\nAmount of " << refund_fee
+    << "is returned into your account.\n5% Service fee of " << service_fee << 
     " is deducted.\n===Keep using JazzCash and enjoy our services===\n";
 }
 int main(){
